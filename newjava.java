559b42e2e618bc44c03218b4bcaf24b33f13295e
Sampath844 <137400186+Sampath844@users.noreply.github.com>
2023-12-30 22:00:12 +0530

    Update newjava.java

--- newjava.java
+++ newjava.java
@@ -8,12 +8,26 @@ class stop {
     int returnDay;
     int timeFromPrev;
 
+    public stop(int pincode, String name, String idealTime, String idealReturnTime, int onDay, int returnDay, int timeFromPrev) {
+        this.pincode = pincode;
+        this.name = name;
+        this.idealTime = idealTime;
+        this.idealReturnTime = idealReturnTime;
+        this.onDay = onDay;
+        this.returnDay = returnDay;
+        this.timeFromPrev = timeFromPrev;
+    }
 
 }
 class Route {
     int uniqueId;
     int journeyLength;
     stop s[];
+    public Route(int uniqueId, int journeyLength, stop[] stops) {
+        this.uniqueId = uniqueId;
+        this.journeyLength = journeyLength;
+        this.s = stops;
+    }
 }
 class links{
     int id;
@@ -27,18 +41,31 @@ class randomidk{
 }
 public class newjava {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-       /* stop[] s = new stop[5];
-        for(int i = 0 ; i<5;i++){
-            s[i] = new stop();
+        stop[] stopsArray1 = {new stop(0, "stop0", "9:00", "17:00", 1, 2, 0),
+                              new stop(1, "stop1", "10:30", "16:30", 1, 2, 1),
+                              new stop(2, "stop2", "12:00", "15:45", 1, 2, 1),
+                              new stop(3, "stop3", "13:30", "14:30", 1, 2, 1),
+                              new stop(4, "stop4", "15:00", "13:45", 1, 2, 1),
+                              new stop(5, "stop5", "16:30", "12:00", 1, 2, 1)};
+        Route route1 = new Route(1, 6, stopsArray1);
 
-        }*/
-        System.out.println("Enter the source city name ");
-        String src = sc.next();
-        System.out.println("Enter Destination city name");
-        String dest = sc.next();
-        System.out.println("Enter the starting time of delivery");
-        int startTime = sc.nextInt();
+        stop[] stopsArray2 = {new stop(6, "stop6", "8:30", "18:00", 1, 2, 0),
+                              new stop(7, "stop7", "10:00", "16:00", 1, 2, 2),
+                              new stop(8, "stop8", "12:30", "15:15", 1, 2, 1),
+                              new stop(4, "stop4", "14:00", "14:45", 1, 2, 1)};
+        Route route2 = new Route(2, 4, stopsArray2);
 
+        stop[] stopsArray3 = {new stop(9, "stop9", "7:45", "18:30", 1, 2, 0),
+                              new stop(10, "stop10", "9:15", "15:30", 1, 2, 1),
+                              new stop(11, "stop11", "11:30", "15:00", 1, 2, 1),
+                              new stop(12, "stop12", "13:45", "13:45", 1, 2, 1),
+                              new stop(13, "stop13", "15:15", "12:30", 1, 2, 1)};
+        Route route3 = new Route(3, 5, stopsArray3);
+
+        stop[] stopsArray4 = {new stop(2, "stop2", "14:30", "12:00", 1, 2, 0),
+                              new stop(8, "stop8", "16:00", "10:30", 1, 2, 2),
+                              new stop(11, "stop11", "18:15", "9:45", 1, 2, 1)};
+        Route route4 = new Route(4, 3, stopsArray4);
     }
 }
+
