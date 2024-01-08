ff691d579ff774893abfb6a2bfa88182816d3dee
Hitesh918 <118730681+Hitesh918@users.noreply.github.com>
2024-01-08 21:38:48 +0530

    Update and rename newjava.java to courierTracker.java

---
+++ courierTracker.java
@@ -0,0 +1,217 @@
+//packages
+
+import java.util.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/*
+mon-1
+tue-2
+wed-3
+thur-4
+fri-5
+sat-6
+sun-7
+*/
+
+//class definitions
+
+class stop {
+    int pincode;
+    String name;
+    String idealTime;
+    String idealReturnTime;
+    int onDay;
+    int returnDay;
+    int timeFromPrev;
+
+    public stop(int pincode, String name, String idealTime, String idealReturnTime, int onDay, int returnDay, int timeFromPrev) {
+        this.pincode = pincode;
+        this.name = name;
+        this.idealTime = idealTime;
+        this.idealReturnTime = idealReturnTime;
+        this.onDay = onDay;
+        this.returnDay = returnDay;
+        this.timeFromPrev = timeFromPrev;
+    }
+
+}
+class Route {
+    int uniqueId;
+    int onEvery;
+    int returnAfter;
+    stop s[];
+    public Route(int uniqueId, int onEvery, int returnAfter ,stop[] stops) {
+        this.uniqueId = uniqueId;
+        this.onEvery = onEvery;
+        this.returnAfter=returnAfter;
+        this.s = stops;
+    }
+}
+class Node {
+    static Node[] allNodes = new Node[5];
+
+    int id;
+    int[] links;
+
+    public Node(int id, int[] links) {
+        this.id = id;
+        this.links = links;
+        allNodes[id] = this; 
+    }
+}
+
+
+public class courierTracker {
+    
+    //function to return all routes in which given stop is present
+    
+    public static List<Integer> findRoutesByPincode(int targetPincode, Route[] routes) {
+        List<Integer> matchingRoutes = new ArrayList<>();
+
+        for (Route route : routes) {
+            for (stop stop : route.s) {
+                if (stop.pincode == targetPincode) {
+                    matchingRoutes.add(route.uniqueId);
+                    break;  
+                }
+            }
+        }
+
+        return matchingRoutes;
+    }
+    
+    //funtion to find all possible ways between 2 routes
+    
+     private static List<List<Integer>> findAllRoutes(Node startNode, int startId, int endId) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<List<Integer>> queue = new LinkedList<>();
+        queue.add(Collections.singletonList(startId));
+
+        while (!queue.isEmpty()) {
+            List<Integer> currentRoute = queue.poll();
+            int currentId = currentRoute.get(currentRoute.size() - 1);
+
+            if (currentId == endId) {
+                result.add(new ArrayList<>(currentRoute));
+            } else {
+                Node currentNode = Node.allNodes[currentId];
+                for (int neighborId : currentNode.links) {
+                    if (!currentRoute.contains(neighborId)) {
+                        List<Integer> newRoute = new ArrayList<>(currentRoute);
+                        newRoute.add(neighborId);
+                        queue.add(newRoute);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+    
+    // function to return number of minutes to wait till the desired time occurs in future
+    
+    public static int calculateMinutesUntilDesiredTime(String startTime, int startDay, String desiredTime, int desiredDay)
+            throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+
+        Date startingTime = sdf.parse(startTime);
+        Date desiredTimeDate = sdf.parse(desiredTime);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(startingTime);
+        calendar.set(Calendar.DAY_OF_WEEK, startDay);
+
+        long currentTimeMillis = calendar.getTimeInMillis();
+        long desiredTimeMillis = calculateDesiredTimeMillis(calendar, desiredTimeDate, desiredDay);
+
+        if (currentTimeMillis > desiredTimeMillis) {
+            calendar.add(Calendar.DAY_OF_WEEK, 7);
+            desiredTimeMillis = calculateDesiredTimeMillis(calendar, desiredTimeDate, desiredDay);
+        }
+
+        long timeDifferenceMillis = desiredTimeMillis - currentTimeMillis;
+        return (int) (timeDifferenceMillis / (60 * 1000)); 
+    }
+
+    private static long calculateDesiredTimeMillis(Calendar calendar, Date desiredTime, int desiredDay) {
+        while (calendar.get(Calendar.DAY_OF_WEEK) != desiredDay) {
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+        }
+
+        calendar.set(Calendar.HOUR_OF_DAY, desiredTime.getHours());
+        calendar.set(Calendar.MINUTE, desiredTime.getMinutes());
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        return calendar.getTimeInMillis();
+    }
+
+    
+    public static void main(String[] args) {
+        stop[] stopsArray1 = {new stop(0, "stop0", "15:00", "11:00", 4, 5, -1),
+                              new stop(1, "stop1", "16:00", "10:00", 4, 5, 60),
+                              new stop(2, "stop2", "17:00", "09:00", 4, 5, 60),
+                              new stop(3, "stop3", "19:00", "07:00", 4, 5, 120),
+                              new stop(4, "stop4", "20:00", "06:00", 4, 5, 60),
+                              new stop(5, "stop5", "21:00", "05:00", 4, 5, 60)};
+        Route route1 = new Route(1, 4, 8 , stopsArray1);
+
+        stop[] stopsArray2 = {new stop(6, "stop6", "20:20", "13:40", 5, 1, -1),
+                              new stop(7, "stop7", "10:30", "23:30", 6, 0, 850),
+                              new stop(8, "stop8", "12:00", "22:00", 6, 0, 90),
+                              new stop(4, "stop4", "17:00", "17:00", 6, 0, 300)};
+        Route route2 = new Route(2, 5, 24 ,  stopsArray2);
+
+        stop[] stopsArray3 = {new stop(9, "stop9", "12:00", "06:00", 4, 5, -1),
+                              new stop(10, "stop10", "14:00", "04:00", 4, 5, 120),
+                              new stop(11, "stop11", "15:00", "03:00", 4, 5, 60),
+                              new stop(12, "stop12", "18:00", "00:00", 4, 5, 180),
+                              new stop(13, "stop13", "20:00", "22:00", 4, 4, 120)};
+        Route route3 = new Route(3, 4 , 2 , stopsArray3);
+
+        stop[] stopsArray4 = {new stop(11, "stop11", "17:30", "06:30", 6, 0, -1),
+                              new stop(8, "stop8", "18:00", "06:00", 6, 0, 30),
+                              new stop(2, "stop2", "19:00", "05:00", 6, 0, 60),
+                              new stop(14, "stop14", "21:00", "03:00", 6, 0, 120)};
+        Route route4 = new Route(4, 6 , 6 ,  stopsArray4);
+        
+        
+        Node node1 = new Node(1, new int[]{2, 4});
+        Node node2 = new Node(2, new int[]{1, 4});
+        Node node3 = new Node(3, new int[]{4});
+        Node node4 = new Node(4, new int[]{1, 2, 3});
+        
+        Route[] routes = {route1, route2, route3, route4};
+        
+        Node[] links = {node1, node2, node3, node4};
+        
+        int targetPincode = 11;
+        List<Integer> matchingRoutes = findRoutesByPincode(targetPincode, routes);
+
+        System.out.println("Routes containing pincode " + targetPincode + ": " + matchingRoutes);
+        
+        int startId = 4;
+        int endId = 1;
+
+        List<List<Integer>> allRoutes = findAllRoutes(node1, startId, endId);
+
+        System.out.println("All possible routes from " + startId + " to " + endId + ":");
+        for (List<Integer> route : allRoutes) {
+            System.out.println(route);
+        }
+        
+        String startTime = "08:30";
+        int startDay = 6; 
+        String desiredTime = "05:45";
+        int desiredDay = 6; 
+
+        try {
+            int minutesUntilDesiredTime = calculateMinutesUntilDesiredTime(startTime, startDay, desiredTime, desiredDay);
+            System.out.println("Minutes until desired time: " + minutesUntilDesiredTime);
+        } catch (ParseException e) {
+            System.out.println("Error parsing time: " + e.getMessage());
+        }
+
+    }
+}
